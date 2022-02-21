80bcf0d45271050d38844d2a1ba0341802a701e5
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 11:04:44 +0100

    Exercise 2 done

--- src/for and if/exercise2/index.js
+++ src/for and if/exercise2/index.js
@@ -1,8 +1,8 @@
 const numbers = [0,1,2,4,5,9,3,6,7,8];
 
-// Put your code here
+let counter=0;
 for (let item of numbers) {
-    // Put your code here
+    counter++;
 }
 
-console.log(/* Put your code here */)
+console.log(counter)
