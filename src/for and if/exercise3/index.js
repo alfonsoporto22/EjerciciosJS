b950b779d9f01d2d1fae68faf0a7ec286fd2ac1e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 11:09:23 +0100

    Exercise 3 done

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -1,7 +1,15 @@
 const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
+let contador=0;
+
 
 // Put your code here
+for (let item of numbers){
+    sum=sum+item;
+    contador++;
+}
 
-console.log(average)
+average=sum/contador;
+
+console.log(average);
