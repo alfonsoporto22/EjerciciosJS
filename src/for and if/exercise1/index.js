e645e7491b37cd161d337242a41ee476cc7a3b9a
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 11:02:52 +0100

    Exercise 1 done

--- src/for and if/exercise1/index.js
+++ src/for and if/exercise1/index.js
@@ -1,11 +1,15 @@
-const numbers = [22,1,4,3,5,7,8,9,12,11];
+const numbers = [22, 1, 4, 3, 5, 7, 8, 9, 12, 11];
 const odd = [];
 const even = []
 
 for (let item of numbers) {
     console.log("item:", item);
 
-    // Put your code here
+    if (item % 2 === 0) {
+        even.push(item);
+    } else {
+        odd.push(item);
+    }
 
 }
 
