ba02f7479e63312c55347dc303c5f7a1e87578b5
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-22 12:11:48 +0100

    Exerxise3 done

--- src/functions/exercise3/index.js
+++ src/functions/exercise3/index.js
@@ -1,7 +1,10 @@
 
-// Put your code here
+function power(number, n=2) {
+    
+    return number ** (1 / n);
+}
 
 console.log(power(25))
-console.log(power(8,1/3))
-console.log(power(2,3))
-console.log(power(5,2))
+console.log(power(8, 1 / 3))
+console.log(power(2, 3))
+console.log(power(5, 2))
