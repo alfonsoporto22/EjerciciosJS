672a72a950f674eb6cef6686daa4662cbb34c694
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 11:47:55 +0100

    Exercise 1 done

--- src/objects as maps/exercise1/index.js
+++ src/objects as maps/exercise1/index.js
@@ -1,15 +1,30 @@
 const phrase = "No hay bien que por mal no venga";
 const counters = {};
+/**
+ * Devuelve el número de veces que aparece una letra
+ * @param {string} letter 
+ * @param {array} countersObject 
+ * @returns {number}
+ */
 function countLetter (letter, countersObject) {
-
-    // Put your code here
+    let contador=0;
+    for(let i=0;i<countersObject.length;i++){
+        if(countersObject[i]==letter){
+            contador++;
+        }
+    }
+    return contador;
 
 }
 
-
+const frase=[];
 for (letter of phrase) {
-    console.log(letter)
+    
+    frase.push(letter);
     countLetter(letter, counters)
 }
+
+console.log(frase);
+console.log(countLetter("m", frase));
 
 console.log(counters)
