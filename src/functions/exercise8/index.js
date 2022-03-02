474186d16bd6cff6efd0070e41cd8b224600f60c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 10:11:23 +0100

    Exercise 8 done

--- src/functions/exercise8/index.js
+++ src/functions/exercise8/index.js
@@ -1,28 +1,31 @@
-numbers = [4,2,9,6,1,0,3,6,5,2,9];
+numbers = [4, 2, 9, 6, 1, 0, 3, 6, 5, 2, 9];
 
-function aGreatherThanB (a, b) {
-    // Put your code here
+
+function aGreatherThanB(a, b) {
+    return a > b;
 }
 
-function bGreatherThanA (a,b) {
-    // Put your code here
+
+function bGreatherThanA(a, b) {
+    return b > a;
 }
 
-function swap (idxA, idxB, array) {
-    // Put your code here
+function swap(idxA, idxB, array) {
+    const temp = array[idxA];
+    array[idxA] = array[idxB];
+    array[idxB] = temp;
 }
 
-function sort (comparator, array) {
-    for ( let currentIdx = 0 ; currentIdx < array.length-1 ; currentIdx++) {
-        for ( let nextIdx = currentIdx+1 ; nextIdx < array.length ; nextIdx++ ) {
+function sort(comparator, array) {
+    for (let currentIdx = 0; currentIdx < array.length - 1; currentIdx++) {
+        for (let nextIdx = currentIdx + 1; nextIdx < array.length; nextIdx++) {
             if (
-                comparator(currentIdx, nextIdx)
-            ) swap (currentIdx, nextIdx, array)
+                comparator(array[currentIdx], array[nextIdx])
+            ) swap(currentIdx, nextIdx, array)
         }
     }
 }
 
-console.log(numbers);
 sort(aGreatherThanB, numbers);
 console.log(numbers);
 sort(bGreatherThanA, numbers);
